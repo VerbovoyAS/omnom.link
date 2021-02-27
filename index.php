9e49b406205412d2c4873640df0f8bf626f08b01
Verbovoi Aleksei <verbovoyas@mail.ru>
2021-02-27 19:58:19 +1000

    check download in git

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="RU">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>omNom.link</title>
+</head>
+<body>
+    <?php echo 'NEW site';?>
+    
+</body>
+</html>
